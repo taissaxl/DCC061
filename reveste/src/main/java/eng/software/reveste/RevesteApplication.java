b41456935922aa031e0293e912ac9f555e2d9e41
Taíssa <ol.taissa.2002@gmail.com>
2025-01-15 11:44:33 -0300

    cria projeto usando spring initializr

---
+++ reveste/src/main/java/eng/software/reveste/RevesteApplication.java
@@ -0,0 +1,13 @@
+package eng.software.reveste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevesteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RevesteApplication.class, args);
+	}
+
+}
---
+++ reveste/src/test/java/eng/software/reveste/RevesteApplicationTests.java
@@ -0,0 +1,13 @@
+package eng.software.reveste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RevesteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
