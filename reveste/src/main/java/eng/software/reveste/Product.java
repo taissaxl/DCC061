51bf14a9b8e004b87cd36039d4c516a8d7be4233
Taíssa <ol.taissa.2002@gmail.com>
2025-01-15 16:57:48 -0300

    add classe de produto

---
+++ reveste/src/main/java/eng/software/reveste/Product.java
@@ -0,0 +1,39 @@
+package eng.software.reveste;
+
+public class Product {
+    private int id;
+    private String name;
+    private String description;
+    private double price;
+    private String imageUrl;
+
+    // Construtor
+    public Product(int id, String name, String description, double price, String imageUrl) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.imageUrl = imageUrl;
+    }
+
+    // Getters
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+}
